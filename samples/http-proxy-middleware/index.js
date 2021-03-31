547ba11fc62ff3301434537a55fbb8ab4c538392
kekekekeke <1657619661@qq.com>
2021-03-31 22:57:45 +0800

    feat: repeat

--- samples/http-proxy-middleware/index.js
+++ samples/http-proxy-middleware/index.js
@@ -18,11 +18,11 @@ const app = express()
 /**
  * Add the proxy to express
  */
-app.use("/users", mpProxy);
+app.use("/mp", mpProxy);
 app.listen(3000)
 
 console.log("[DEMO] Server: listening on port 3000");
-console.log("[DEMO] Opening: http://localhost:3000/users");
+console.log("[DEMO] Opening: http://localhost:3000/mp");
 
-// 直接打开 http://localhost:3000/users 即可访问到 mp
-// require("open")("http://localhost:3000/users");
+// 直接打开 http://localhost:3000/mp 即可访问到 mp
+// require("open")("http://localhost:3000/mp");
